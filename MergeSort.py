b4582f8045fb09b1efcc95617b18435826eaaeb0
vipulshah31120 <75607031+vipulshah31120@users.noreply.github.com>
2021-05-11 14:04:31 +0530

    DataStructure

--- MergeSort.py
+++ MergeSort.py
@@ -5,7 +5,7 @@
 # Second subarray is arr[m+1..r]
 def merge(arr, l, m, r):
 	n1 = m - l + 1
-	n2 = r- m
+	n2 = r - m
 
 	# create temp arrays
 	L = [0] * (n1)
---
+++ Stack.py
@@ -0,0 +1,15 @@
+stack = []
+
+stack.append('A')
+stack.append('B')
+stack.append('C')
+
+print('Initial Stack: ', stack)
+
+print('Elements poped from Stack: ')
+print(stack.pop())
+print(stack.append('1'))
+print(stack.append('2'))
+print(stack.pop())
+
+print('Final Stack: ', stack)
