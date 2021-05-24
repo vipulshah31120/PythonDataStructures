cbe100f7f07183a4528305641d9428e496067ac4
vipulshah31120 <75607031+vipulshah31120@users.noreply.github.com>
2021-05-24 13:57:52 +0530

    JugglingAlgoForArrayRotation

--- JugglingAlgo.py
+++ JugglingAlgo.py
@@ -1,20 +1,20 @@
-#d = elements
+#d = by which number we want to rotate
 # Function to left rotate arr[] of size n by d
 
 def leftrotate(arr, d, n) :
-    g_c_d = gcd(d, n)
+    g_c_d = gcd(d, n)               #d = d % n
     for i in range (g_c_d) :
         temp = arr[i]                # move i-th values of blocks
         j = i
         while 1 :
-            k = j+d
+            k = j+d                  # i, j, k are positions of the elements in Array
             if k>= n :               # The element have to be rotated
                 k = k-n
             if k == i :              # The element is already in rotated position
                 break
-            arr[j] = arr[k]
+            arr[j] = arr[k]         # elements get swapped
             j = k
-        arr[j] = temp
+        arr[j] = temp               # after all the iterations in while loop, value of j is retrieved  from temp
 
 def printarray(arr, size) :
     for i in range (size) :
